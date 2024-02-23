a5a49f8bd59afa21184e9d09fd8194ce3157b9bf
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-02-23 15:17:24 -0600

    complete exercise 13.7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,12 +10,8 @@ router.get('/', async (req, res) => {
 })
   
 router.post('/', async (req, res) => {
-    try {
         const blog = await Blog.create(req.body)
         return res.json(blog)
-    } catch(error) {
-        return res.status(400).json({ error })
-    }
 })
 
 const blogFinder = async (req, res, next) => {
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 
 const { PORT } = require('./util/config')
@@ -9,6 +10,17 @@ const blogsRouter = require('./controllers/blogs')
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error) {
+    return response.status(400).send({ error: 'something went wrong' })
+  } 
+  next(error)
+}
+
+app.use(errorHandler)
 
 const start = async () => {
   await connectToDatabase()
