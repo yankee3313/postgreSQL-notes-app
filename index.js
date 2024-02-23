a7e884f93149557fd87c915a6bcbd6834c570968
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-02-22 20:52:03 -0600

    complete exercise 13.4

--- index.js
+++ index.js
@@ -13,6 +13,82 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   }
 });
 
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+
+  console.log(JSON.stringify(blogs))
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.get('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    console.log(blog.toJSON())
+    res.json(blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.put('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    blog.author = req.body.author
+    blog.url= req.body.url
+    blog.title = req.body.title
+    blog.likes = req.body.likes
+    await blog.save()
+    res.json(blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
 const main = async () => {
   try {
     await sequelize.authenticate()
@@ -25,51 +101,3 @@ const main = async () => {
 }
 
 main()
-
-// class Blog extends Model {}
-// Blog.init({
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   author: {
-//     type: DataTypes.TEXT
-//   },
-//   url: {
-//     type: DataTypes.TEXT,
-//     allowNull: false
-//   },
-//   title: {
-//     type: DataTypes.TEXT,
-//     allowNull: false
-//   },
-//   likes: {
-//     type: DataTypes.INTEGER,
-//     defaultValue: 0
-//   }
-// }, {
-//   sequelize,
-//   underscored: true,
-//   timestamps: false,
-//   modelName: 'blog'
-// })
-
-// app.get('/api/blogs', async (req, res) => {
-//   const blogs = await Blog.findAll()
-//   res.json(blogs)
-// })
-
-// app.post('/api/blogs', async (req, res) => {
-//   try {
-//     const blog = await Blog.create(req.body)
-//     return res.json(blog)
-//   } catch(error) {
-//     return res.status(400).json({ error })
-//   }
-// })
-
-// const PORT = process.env.PORT || 3001
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
