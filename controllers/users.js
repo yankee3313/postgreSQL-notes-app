164048a01c00b91960574ec8df33590365709120
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-02-23 22:18:47 -0600

    complete exercise 13.8

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.get('/:username', async (req, res) => {
+    const user = await User.findOne({
+        where: { username: req.params.username }
+    });
+    if (user) {
+      res.json(user)
+    } else {
+      res.status(404).end()
+    }
+})
+
+router.put('/:username', async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: req.params.username
+        }
+    })
+    if (user) {
+        user.username = req.body.username
+        await user.save()
+      res.json(user)
+    } else {
+      res.status(404).end()
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
