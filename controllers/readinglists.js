07d5d975be6206c15ad37c2a2b6c450e39727b80
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-03-01 16:32:29 -0600

    complete exercise 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
 
-const { ReadingListBlogs } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
+
+const { ReadingListBlogs, User } = require('../models')
   
 router.post('/', async (req, res) => {
     try {
@@ -10,6 +13,18 @@ router.post('/', async (req, res) => {
     catch(error) {
         return res.status(400).json({ error })
   }
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+    const user = await User.findByPk(req.decodedToken.id)
+    const readingListBlog = await ReadingListBlogs.findByPk(req.params.id)
+    if (readingListBlog && user.id === readingListBlog.userId) {
+        readingListBlog.readCheck = req.body.readCheck
+        await readingListBlog.save()
+        res.json(readingListBlog)
+    } else {
+        res.status(403).json({error: 'You do not have permssion to modify this'})
+    }
 })
 
 module.exports = router
